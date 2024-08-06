cda4cc66253a4241cd4e97dd0eb56ba1d65f36ed
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2024-08-06 19:24:20 +0200

    added simple welcome view

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# myapp/views.py
+from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import Item
+from .serializers import ItemSerializer
 
-# Create your views here.
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+def home(request):
+    return HttpResponse("<h1>Welcome to the API this </h1><p>Navigate to <a href='/api/items/'>/api/items/</a> to use the API.</p>")
